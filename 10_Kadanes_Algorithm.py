2a6ccc8c6bb6cff2eb6db55f414a3302f72a4e42
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2024-12-29 20:43:48 +0530

    day 10

---
+++ 10_Kadanes_Algorithm.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self, arr):
+        n = len(arr)
+        maxSum = arr[0]  # Initialize to the first element
+        currentSum = 0
+        
+        for i in range(n):
+            currentSum += arr[i]
+            if currentSum > maxSum:
+                maxSum = currentSum
+            if currentSum < 0:  # Reset currentSum if it drops below 0
+                currentSum = 0
+                
+        return maxSum
+
+# Main execution
+if __name__ == "__main__":
+    s = Solution()
+    arr = [2, 3, -8, 7, -1, 2, 3]  # Input array
+    print("Maximum Subarray Sum:", s.maxSubArraySum(arr))  # Print the result
