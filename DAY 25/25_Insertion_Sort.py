28fa6254574a321f0c2fdeeb09ed883b3776f4d1
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2025-01-12 10:03:06 +0530

    DAY 25

---
+++ DAY 25/25_Insertion_Sort.py
@@ -0,0 +1,21 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]  
+        j = i - 1
+
+
+        while j >= 0 and arr[j] > key:
+            print(f"Shifting {arr[j]} to position {j + 1}")
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        print(f"Inserting {key} at position {j + 1}")
+        print("Array after this step:", arr)
+
+    return arr
+
+if __name__ == "__main__":
+    data = [12, 11, 13, 5, 6]
+    print("Original array:", data)
+    sorted_array = insertion_sort(data)
+    print("Sorted array:", sorted_array)
